2289f3ef7da719ce51d75f786e727c1ec935abe7
Iskandar Mamadnazarov <iskandarko@gmail.com>
2021-01-05 09:38:08 +0300

    BF: Invalid Host header

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
