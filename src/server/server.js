08c65616811f47ebaeb8a5f27693935a1bf9fd52
Iskandar Mamadnazarov <iskandarko@gmail.com>
2021-01-05 10:48:16 +0300

    BF: Invalid Host header : changing the proxy instance in the package.json

--- src/server/server.js
+++ src/server/server.js
@@ -6,9 +6,9 @@ const port = 8000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.get('/db-request', (req, res) => {
     console.log('Request to DB is handled');
