101bf991a6e7d28f6a36ffecc8de8ea2d611237d
Iskandar Mamadnazarov <iskandarko@gmail.com>
2021-01-05 10:25:26 +0300

    Configuring server app to run properly

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log("Server is listening on port " + port);
+const port = 8000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/db-request', (req, res) => {
     console.log('Request to DB is handled');
     const data = {"data": "Here is some data from the server"};
     res.send(data);
+});
+
+app.listen(port, () => {
+    console.log("Server is listening on port " + port);
 });
